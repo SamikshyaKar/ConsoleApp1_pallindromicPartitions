23a0fc3ba00da43758942c0fc0f3a585945e095a
samikshya <ssamikshya.kar@gmail.com>
2022-03-01 09:02:16 +0530

    -- Full Diamond Pyramid

--- ConsoleApp1_pallindromicPartitions/DiamondPyramid.cs
+++ ConsoleApp1_pallindromicPartitions/DiamondPyramid.cs
@@ -15,30 +15,68 @@ namespace ConsoleApp1_pallindromicPartitions
             Console.WriteLine("Enter the Number of Rows ");
             N = Convert.ToInt32(Console.ReadLine());
 
-            for(i=0;i<=N;i++)
+            for (i = 0; i <= N; i++)
             {
-                for(j=1;j<=N-i;j++)
+                for (j = 1; j <= N - i; j++)
                 {
                     Console.Write(" ");
                 }
-                for(j=1;j<=i;j++)
+                for (j = 1; j <= i; j++)
                 {
                     Console.Write("{0}", j);
                 }
-                for (j=i-1;j>=1;j--)
+                for (j = i - 1; j >= 1; j--)
                 {
                     Console.Write("{0}", j);
                 }
                 Console.Write("\n");
             }
+        }
+
+            public void DiamondPyramidmethodFull()
+            {
+                Console.WriteLine("Enter the Number of Rows ");
+                N = Convert.ToInt32(Console.ReadLine());
+
+                for (i = 0; i <= N; i++)
+                {
+                    for (j = 1; j <= N - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (j = 1; j <= i; j++)
+                    {
+                        Console.Write("{0}", j);
+                    }
+                    for (j = i - 1; j >= 1; j--)
+                    {
+                        Console.Write("{0}", j);
+                    }
+                    Console.Write("\n");
+                }
+
+                for (i = N-1; i >=1; i--)
+                {
+                    for (j = 1; j <= N - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (j = 1; j <= i; j++)
+                    {
+                        Console.Write("{0}", j);
+                    }
+                    for (j = i - 1; j >= 1; j--)
+                    {
+                        Console.Write("{0}", j);
+                    }
+                    Console.Write("\n");
+                }
+
+            }
+
+
 
 
 
         }
-       
-
-    
-
-
-    }
 }
